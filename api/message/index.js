f7adac63208575a2d470461e3451edfa4b5e784c
Lukas <lukj@itu.dk>
2022-06-24 09:48:52 +0200

    Added ryan

--- api/message/index.js
+++ api/message/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, req) {
     //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res.json({
-        text: "Hello Lukas from the API"
+        text: "Hello Ryan from the API"
     })
     // // status: 200, /* Defaults to 200 */
     // body: responseMessage
